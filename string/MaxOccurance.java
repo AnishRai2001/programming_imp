9b7b400b2729c81f0461932b658267a3c32647fe
Anish Rai <anishrai2023ee@gmail.com>
2025-10-18 19:23:22 +0530

    maxOccurance

---
+++ string/MaxOccurance.java
@@ -0,0 +1,30 @@
+package programming.string;
+
+import java.util.HashMap;
+
+public class MaxOccurance {
+
+	public static void main(String[] args) {
+		System.out.println(maxConsecutive("It's a animal"));
+	}
+	public static char maxConsecutive(String str) {
+		int max=0;
+		char maxOccurance=' ';
+		  HashMap<Character, Integer> map = new HashMap<>();
+	 
+	  for(int i=0;i<str.length();i++) {
+		  if(map.containsKey(str.charAt(i))) {
+			  map.put(str.charAt(i), map.get(str.charAt(i))+1);
+		  }
+		  else
+			  map.put(str.charAt(i),1);
+	  }
+	  for(Character c:map.keySet()) {
+		  if(map.get(c)>max) {
+			  max=map.get(c);
+			  maxOccurance = c;
+		  }
+	  }
+	  return maxOccurance;
+}
+}
