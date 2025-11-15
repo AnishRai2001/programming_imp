b0f444bb3f5aeac5d42672e4d2f144a35eade47f
Anish Rai <anishrai2023ee@gmail.com>
2025-11-15 23:31:30 +0530

    itterate

---
+++ programingImpl/IterrateArrayList.java
@@ -0,0 +1,17 @@
+
+package programingImpl;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IterrateArrayList {
+	public static void main(String[] args) {
+		
+	
+	List<String>l1=Arrays.asList("java","Anish","good");
+	for(String s:l1) {
+		System.out.println(s);
+	}
+
+}
+}
